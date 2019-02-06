8ed2c1e48cef134a6a73050ada70859c9ff1a122
michael-a-santorelli <46325681+michael-a-santorelli@users.noreply.github.com>
2019-02-06 14:23:52 -0500

    Update DownSample.m

--- DownSample.m
+++ DownSample.m
@@ -7,7 +7,7 @@ y2 = y(:, 2);
 
 if(pf == 0)     % if pf flag is 0 do not use pre-filter
     DownSampled = downsample(y, N);     
-else        % if pf flag is 1 not use pre-filter
+else        % if pf flag is 1 use pre-filter
     DownSampled(:,1) = decimate(y1, N);
     DownSampled(:,2) = decimate(y2, N);
 end
